e6ae6d88139cc84af56b89bb2b03ce2b41b0f9a8
rtaylor205 <rtaylor205@84f797ac-9f24-11de-b98f-4500b42dc123>
2010-06-24 09:25:20 +0000

    Polygon.java is no longer a drawable object, it will be a generic convex polygon shape (not directly compatible with Box2D, but conversion methods will be added). This will be used for the base of a drawable polygon, or assigned to a Sprite for non-Box2D collision detection
    git-svn-id: http://rokon.googlecode.com/svn/trunk@268 84f797ac-9f24-11de-b98f-4500b42dc123

--- src/com/stickycoding/rokon/Polygon.java
+++ src/com/stickycoding/rokon/Polygon.java
@@ -4,42 +4,12 @@ import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Polygon.java
- * A drawable polygon shape
+ * A polygon shape
  * 
  * @author Richard
  */
-public class Polygon extends BasicGameObject implements Drawable {
 
-	public Polygon(float x, float y, float width, float height) {
-		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
-	}
+public class Polygon {
 
-	public boolean isAlive() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean isOnScreen() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean isTouchable() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public void onAdd(Layer layer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onDraw(GL10 gl) {
-		// TODO Auto-generated method stub
-		
-	}
 	
-	
-
 }

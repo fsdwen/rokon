16400e16b1e66db04174b75afb94164f5fd11389
rtaylor205 <rtaylor205@84f797ac-9f24-11de-b98f-4500b42dc123>
2010-06-24 14:06:43 +0000

    Polygon.java can hold up to 128 vertices, ready for use elsewhere
    git-svn-id: http://rokon.googlecode.com/svn/trunk@271 84f797ac-9f24-11de-b98f-4500b42dc123

--- src/com/stickycoding/rokon/Polygon.java
+++ src/com/stickycoding/rokon/Polygon.java
@@ -1,15 +1,46 @@
 package com.stickycoding.rokon;
 
-import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Polygon.java
- * A polygon shape
+ * A polygon shape, vertices should be from 0 to 1
  * 
  * @author Richard
  */
 
 public class Polygon {
+	
+	public static final int MAX_VERTEX_COUNT = 128;
 
+	protected boolean building;
+	protected Point[] vertex;
+	protected int vertexCount = 0;
+	
+	public Polygon(Point[] vertices) {
+		this.vertex = vertices;
+		vertexCount = vertices.length;
+	}
+	
+	public Polygon() {
+		vertex = new Point[MAX_VERTEX_COUNT];
+		vertexCount = 0;
+	}
+	
+	public void addVertex(Point location) {
+		vertex[vertexCount++] = location;
+	}
+	
+	public void addVertex(float x, float y) {
+		addVertex(new Point(x, y));
+	}
+	
+	public Point getVertex(int index) {
+		return vertex[index];
+	}
+	
+	public int getVertexCount() {
+		return vertexCount;
+	}
+	
 	
 }

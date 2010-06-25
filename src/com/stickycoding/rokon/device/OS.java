c87b5969cbf04736a553462212533e730f4cade6
rtaylor205 <rtaylor205@84f797ac-9f24-11de-b98f-4500b42dc123>
2010-06-25 10:13:59 +0000

    Updated /device/OS.java
    git-svn-id: http://rokon.googlecode.com/svn/trunk@274 84f797ac-9f24-11de-b98f-4500b42dc123

--- src/com/stickycoding/rokon/device/OS.java
+++ src/com/stickycoding/rokon/device/OS.java
@@ -9,28 +9,38 @@ public class OS {
 	public static void determineAPI() {
 		if(Build.VERSION.SDK == "1") {
 			API_LEVEL = 1;
+			return;
 		}
 		if(Build.VERSION.SDK == "2") {
 			API_LEVEL = 2;
+			return;
 		}
 		if(Build.VERSION.SDK == "3") {
 			API_LEVEL = 3;
+			return;
 		}
 		if(Build.VERSION.SDK == "4") {
 			API_LEVEL = 4;
+			return;
 		}
 		if(Build.VERSION.SDK == "5") {
 			API_LEVEL = 5;
+			return;
 		}
 		if(Build.VERSION.SDK == "6") {
 			API_LEVEL = 6;
+			return;
 		}
 		if(Build.VERSION.SDK == "7") {
 			API_LEVEL = 7;
+			return;
 		}
 		if(Build.VERSION.SDK == "8") {
 			API_LEVEL = 8;
+			return;
 		}
+		API_LEVEL = Integer.parseInt(Build.VERSION.SDK);
+		Debug.warning("UNKNOWN API LEVEL " + Build.VERSION.SDK);
 	}
 
 }
